9d07876834aea78e93669dfa15a7bc21e0bbd322
Wyrdrawere <33810800+Wyrdrawere@users.noreply.github.com>
2020-06-10 17:20:54 +0200

    3e not yet working

--- Assignment 3/matlab/f3e.m
+++ Assignment 3/matlab/f3e.m
@@ -1,4 +1,23 @@
 function [l2dist] = f3e(mu_n, n, samples)
 
+[l2dist] = zeros(1,6);
+
+g = @(x, mu, var) ...
+    ( 1 / ( sqrt( 2*pi*var )) * exp( -( x - mu ).^2 / ( 2*var ) ) );
+
+var = 1;
+
+var_0 = 1;
+     
+
+N = @(x) g(x, 3, 1);
+
+B = @(i) @(x) g(x, mu_n(i), (var_0*var) / (n(i)*var_0+var));
+
+for i = 1:length(n)
+    l2dist(i) = sqrt(dot(N-B(i),B(i)-N));
+end
+
+
 
 end
