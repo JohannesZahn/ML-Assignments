eee5bf00618676c29844214d2f7bc941553c2135
Tanja Heck <tanja.heck@student.uni-tuebingen.de>
2020-06-24 12:40:04 +0200

    Added A5
    Added A5 from the Overleaf upload.

--- Assignment_04/matlab/f5b.m
+++ Assignment_04/matlab/f5b.m
@@ -1,5 +1,21 @@
 function [mu1, mu2, scatter_w, scatter_b] = f5b(c1, c2)
 
+mu1 = mean(c1);
+mu2 = mean(c2);
 
+scatterW1 = getScatter(c1);
+scatterW2 = getScatter(c2);
+
+scatter_w = scatterW1 + scatterW2;
+scatter_b = (mu1-mu2)' * (mu1-mu2);      %Transpos. invertiert, VL arbeitet mit Zeilen, hier Spaltenvektoren
+end
+
+function scatter = getScatter(c)
+m = mean(c);
+scatter = 0;
+for i=1:size(c,1)   %for every row in data
+    x = c(i,:);     %get x
+    scatter = scatter + ( (x-m)' * (x-m) );     %compute scatter sum over all x         %invertiert s.o.
+end
 
 end
--- Assignment_04/matlab/f5c.m
+++ Assignment_04/matlab/f5c.m
@@ -1,5 +1,10 @@
 function [c1_proj, c2_proj] = f5c(c1, c2, mu1, mu2, scatter_w)
+w = inv(scatter_w) * (mu1-mu2)';         %calculate w        %transpos. erneut invertiert (s.o.)
+w = w/norm(w);       %normalize w
+m = [1/2*(mu1(1)+mu2(1)) , 1/2*(mu1(2)+mu2(2))];        %get global mean from both class means
 
-
-
+%project each element of both classes on the line defined by the vector
+%between the global mean and w
+c1_proj = (((c1 - m) * w) .* w' )+m;
+c2_proj = (((c2 - m) * w) .* w' )+m;
 end
